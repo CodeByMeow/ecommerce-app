b6e9e3be69dc1f8bd3a32e5b28f5b0c5a050fa52
katinbox <tranquanki28@gmail.com>
2022-10-29 15:19:11 +0700

    feat: upload image to cloudinary

--- routes/image.js
+++ routes/image.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
 const { uploadImageToLocal } = require("../middlewares/uploadImageLocal");
+const { cloudUploadService } = require("../services/cloudUploadService");
 
-router.post("/upload", uploadImageToLocal, (req, res) => {
-    res.json({
-        msg: "image uploaded",
-    });
+router.post("/upload", uploadImageToLocal, async (req, res) => {
+    const { path } = req.file;
+
+    try {
+        const uploadRespose = await cloudUploadService(
+            path,
+            process.env.CLOUDINARY_UPLOAD_FOLDER
+        );
+
+        const { secure_url } = uploadRespose;
+        fs.unlink(path, (err) => {
+            if (err) throw err;
+            console.log(`info: ${path} was deleted`);
+        });
+
+        res.json({
+            msg: "upload successfully",
+            data: {
+                url: secure_url,
+            },
+        });
+    } catch (err) {
+        throw new Error(err);
+    }
 });
 
 module.exports = router;
--- services/cloudUploadService.js
+++ services/cloudUploadService.js
@@ -2,7 +2,23 @@ const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
     api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUNDINARY_SECRET_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY,
     cloud_name: process.env.CLOUDINARY_NAME,
     secure: true,
 });
+
+const cloudUploadService = (file, folder) => {
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.upload(file, { folder }, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+};
+
+module.exports = {
+    cloudUploadService,
+};
