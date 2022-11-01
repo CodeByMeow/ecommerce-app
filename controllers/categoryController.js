906f702a83dffe4c0d48c33eecaf0fc9e6397eba
katinbox <tranquanki28@gmail.com>
2022-11-01 14:39:40 +0700

    feat: category create with auto index general same slug

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,24 +1,41 @@
+const { default: slugify } = require("slugify");
 const CategoryModel = require("../models/category");
 
 const getById = (id) => {
     return CategoryModel.findById(id);
 };
 
-const getAll = () => {
-    return CategoryModel.find();
+const getAll = (param) => {
+    return CategoryModel.find(param);
 };
 
 const findOne = (param) => {
     return CategoryModel.findOne(param);
 };
 
-const create = (category) => {
-    const newCategory = new CategoryModel(category);
+const generalSlug = (title) => {
+    return slugify(title);
+};
+
+const findBySlug = (slug) => {
+    return getAll({ slug });
+};
+
+const create = async (category) => {
+    let slug = generalSlug(category.title);
+    const sameSlug = await findBySlug(slug);
+    if (sameSlug.length > 0) {
+        const tail = sameSlug.length + 1;
+        slug = `${slug}-${tail}`;
+    }
+    const newCategory = new CategoryModel({ ...category, slug });
     return newCategory.save();
 };
+
 module.exports = {
     getById,
     getAll,
     findOne,
     create,
+    findBySlug,
 };
--- routes/category.js
+++ routes/category.js
@@ -3,11 +3,25 @@ const router = express.Router();
 
 const validateInput = require("../middlewares/validate-input");
 const categorySchema = require("../validateSchema/categorySchema.json");
+const categoryController = require("../controllers/categoryController");
 
-router.post("/", validateInput(categorySchema), (req, res) => {
-    res.json({
-        msg: "Create category successfully",
-    });
+router.post("/", validateInput(categorySchema), async (req, res) => {
+    const { title, shortDesc, longDesc, image_url } = req.body;
+    const newCategory = {
+        title,
+        shortDesc,
+        longDesc,
+        image_url,
+    };
+
+    try {
+        await categoryController.create(newCategory);
+        return res.json({
+            msg: "Create category successfully",
+        });
+    } catch (error) {
+        throw new Error(error.message);
+    }
 });
 
 module.exports = router;
