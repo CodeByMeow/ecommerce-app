0f097b23d0d715f98f126ae5ddefedba24171dc5
katinbox <tranquanki28@gmail.com>
2022-11-03 10:49:14 +0700

    feat: router create product

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const userRouter = require("./account");
 const imageRouter = require("./image");
 const categoryRouter = require("./category");
+const productRouter = require("./product");
 
 router.use("/account", userRouter);
 router.use("/images", imageRouter);
 router.use("/category", categoryRouter);
+router.use("/products", productRouter);
 
 module.exports = router;
