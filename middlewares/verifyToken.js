0ce71ffc7a3bb716479f1d778026362379200061
katinbox <tranquanki28@gmail.com>
2022-11-09 16:46:39 +0700

    fix: conflict token decoded

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -5,16 +5,15 @@ const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 module.exports = async (req, res, next) => {
     const token = req.headers[ACCESS_TOKEN_KEY];
-    // console.log(token);
-    if(!token) {
+    if (!token) {
         return res.status(400).json({
-            msg:"Access token is required"
+            msg: "Access token is required",
         });
     }
     try {
         const decoded = jwt.verify(token, JWT_SECRET_KEY);
         if (decoded) {
-            req.user = decoded;
+            req.decoded = decoded;
             next();
         }
     } catch (err) {
--- routes/account.js
+++ routes/account.js
@@ -220,7 +220,7 @@ router.post("/login", async (req, res) => {
  *                              $ref: '#/components/schemas/UserResponse'
  */
 router.get("/profile", verifyTokenMdw, async (req, res) => {
-    const { user_id } = req.user;
+    const { user_id } = req.decoded;
 
     try {
         const user = await UserController.findUserById(user_id);
@@ -229,7 +229,7 @@ router.get("/profile", verifyTokenMdw, async (req, res) => {
             data: user,
         });
     } catch (err) {
-        next(err);
+        throw new Error(err.message);
     }
 });
 
@@ -310,7 +310,7 @@ router.post("/token", async (req, res) => {
                 const response = {
                     token,
                 };
-                console.log("new Token: ", token);
+
                 return res.json(response);
             }
         } catch (error) {
