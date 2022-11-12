445af42aa6c72a9bcf06c6932edc2cbb107e6001
katinbox <tranquanki28@gmail.com>
2022-11-12 16:55:16 +0700

    feat: getDetail method in productService

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -4,6 +4,9 @@ const productService = {
     getList: (params = {}) => {
         return axiosInstance.get("/products", { params });
     },
+    getDetail: (slug) => {
+        return axiosInstance.get(`${slug}`);
+    },
 };
 
 export default productService;
