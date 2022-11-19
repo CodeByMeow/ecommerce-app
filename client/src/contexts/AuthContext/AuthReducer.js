642624eba06d23b1314714a3bd2aca660c59dc71
katinbox <tranquanki28@gmail.com>
2022-11-19 20:43:30 +0700

    fix: clear cart when user logout

--- client/src/contexts/AuthContext/AuthReducer.js
+++ client/src/contexts/AuthContext/AuthReducer.js
@@ -1,47 +1,50 @@
-import { SIGN_OUT, SIGN_IN, SIGN_UP, GET_USER_INFO, REFRESH_TOKEN } from "../types";
+import {
+    SIGN_OUT,
+    SIGN_IN,
+    SIGN_UP,
+    GET_USER_INFO,
+    REFRESH_TOKEN,
+} from "../types";
 
 const authReducer = (state, action) => {
-  const { type, payload } = action;
-  switch (type) {
-    case SIGN_IN:
-      const { token, refreshToken, isAuthenticated, user } = payload;
-      console.log(payload);
-      localStorage.setItem("token", token);
-      localStorage.setItem("refreshToken", refreshToken);      
-      // localStorage.setItem("orders", JSON.stringify(user.orders));
-      return { ...state, token, refreshToken, isAuthenticated, user };
-    case SIGN_UP:
-      return state;
-    case SIGN_OUT:
-      localStorage.removeItem("refreshToken");
-      localStorage.removeItem("token");
-      return {
-        ...state,
-        token: null,
-        refreshToken: null,
-        isAuthenticated: false,
-        user: null,
-      };
-    case GET_USER_INFO: {
-      const { user } = payload;
-      // console.log(payload);
-      return {
-        ...state,
-        user: user,
-      };
+    const { type, payload } = action;
+    switch (type) {
+        case SIGN_IN:
+            const { token, refreshToken, isAuthenticated, user } = payload;
+            localStorage.setItem("token", token);
+            localStorage.setItem("refreshToken", refreshToken);
+            return { ...state, token, refreshToken, isAuthenticated, user };
+        case SIGN_UP:
+            return state;
+        case SIGN_OUT:
+            localStorage.removeItem("refreshToken");
+            localStorage.removeItem("token");
+            localStorage.removeItem("cart");
+            return {
+                ...state,
+                token: null,
+                refreshToken: null,
+                isAuthenticated: false,
+                user: null,
+            };
+        case GET_USER_INFO: {
+            const { user } = payload;
+            return {
+                ...state,
+                user: user,
+            };
+        }
+        case REFRESH_TOKEN: {
+            const { token } = payload;
+            localStorage.setItem("token", token);
+            return {
+                ...state,
+                token,
+            };
+        }
+        default:
+            return state;
     }
-    case REFRESH_TOKEN: {
-      // console.log(payload);
-      const { token} = payload;      
-      localStorage.setItem("token", token);
-      return {
-        ...state,
-        token,
-      };
-    }
-    default:
-      return state;
-  }
 };
 
 export default authReducer;
