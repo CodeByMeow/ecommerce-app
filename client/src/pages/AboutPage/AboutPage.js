93413da1c4d6121e0432f609e78f90cff183660e
phattruon <108143277+phattruon@users.noreply.github.com>
2022-11-19 10:38:31 +0700

    update about

--- client/src/pages/AboutPage/AboutPage.js
+++ client/src/pages/AboutPage/AboutPage.js
@@ -1,21 +1,12 @@
 import React from "react";
-<<<<<<< HEAD
-import "./About.css";
-import PageContainer from "../../layouts/PageContainer/PageContainer";
-=======
 import PageContainer from "../../layouts/PageContainer/PageContainer";
 import "./About.css";
->>>>>>> feature/phat
 
 const AboutPage = () => {
   return (
     <PageContainer title="About us">
       <div className="About flex flex-col items-center justify-center">
-<<<<<<< HEAD
-        <h1 className="border-b-4 border-indigo-500 m-8">About Us</h1>
-=======
         <h1 className="border-b-4 border-violet-600 m-8">About Us</h1>
->>>>>>> feature/phat
         <p>
           When it comes to smartphones, we’re the only one place that does it
           all.
@@ -44,27 +35,12 @@ const AboutPage = () => {
           <ul className="flex flex-row flex-wrap items-center justify-center">
             <li>
               <img src="https://s3n.cashify.in/imageLibrary/15_lakh_customer_500baeebe850.png"></img>
-<<<<<<< HEAD
-              <span>1.4 Cr+</span>
-              <span>Customer</span>
-=======
               <span>14.000</span>
               <span>khách hàng</span>
->>>>>>> feature/phat
             </li>
             <li>
               <img src="https://s3n.cashify.in/imageLibrary/cash_efc77232393e.png"></img>
               <span>Lakh+</span>
-<<<<<<< HEAD
-              <span>Devices</span>
-              <span>Bought</span>
-            </li>
-            <li>
-              <img src="https://s3n.cashify.in/imageLibrary/cash_efc77232393e.png"></img>
-              <span>250 Cr+</span>
-              <span>Cash</span>
-              <span>Given</span>
-=======
               <span>Dịch vụ</span>
               <span>thanh toán</span>
             </li>
@@ -72,74 +48,38 @@ const AboutPage = () => {
               <img src="https://s3n.cashify.in/imageLibrary/cash_efc77232393e.png"></img>
               <span>2.500</span>
               <span>Tài khoản</span>
->>>>>>> feature/phat
             </li>
             <li>
               <img src="https://s3n.cashify.in/imageLibrary/Offline_Stores_13a0b9059bcb.png"></img>
               <span>150+</span>
-<<<<<<< HEAD
-              <span>Office</span>
-              <span>Store</span>
-=======
               <span>Văn phòng</span>
               <span>Cửa hàng</span>
->>>>>>> feature/phat
             </li>
             <li>
               <img src="https://s3n.cashify.in/imageLibrary/1_Mn_10aad13ff451.png"></img>
               <span>10 Mn+</span>
               <span>App</span>
-<<<<<<< HEAD
-              <span>Dowload</span>
-=======
               <span>Lượt tải</span>
->>>>>>> feature/phat
             </li>
             <li>
               <img src="https://s3n.cashify.in/imageLibrary/10_Lakh_387a3153d208.png"></img>
               <span>40</span>
-<<<<<<< HEAD
-              <span>Lakh</span>
-              <span>Devices</span>
-              <span>Sold</span>
-=======
               <span>Thiết bị</span>
               <span>đã bán</span>
->>>>>>> feature/phat
             </li>
             <li>
               <img src="https://s3n.cashify.in/imageLibrary/Serviceable_locations_c06cb8ac9185.png"></img>
               <span>15000+</span>
-<<<<<<< HEAD
-              <span>Serviceable</span>
-              <span>Locations</span>
-=======
               <span>Địa điểm sửa chữa</span>
->>>>>>> feature/phat
             </li>
             <li>
               <img src="https://s3n.cashify.in/imageLibrary/Services_d9a04f70e9d8.png"></img>
               <span>1000+</span>
-<<<<<<< HEAD
-              <span>Partner</span>
-=======
               <span>Người đồng hành</span>
->>>>>>> feature/phat
             </li>
           </ul>
         </div>
         <div className="flex flex-col history justify-center items-center">
-<<<<<<< HEAD
-          <h1 className="m-8 text-center">Our history</h1>
-
-          <div className="flex flex-row w-3/4 justify-center items-center m-6">
-            <div className="w-3/5">
-              <h3 className="text-teal-400">The IDEA originated in 2013</h3>
-              <p>
-                When one of 3 friends wanted to buy the latest iPhone, he tried
-                to sell off his then-current Samsung phone for funding but found
-                it difficult to do so.
-=======
           <h1 className="m-8 text-center">Lịch sử </h1>
           <div className="flex flex-row flex-wrap w-3/4 justify-center items-center m-6">
             <div className="w-3/5">
@@ -148,34 +88,10 @@ const AboutPage = () => {
                 Khi một trong 3 người bạn muốn mua chiếc iPhone mới nhất, anh ấy
                 đã thử để bán chiếc điện thoại Samsung hiện tại của mình để lấy
                 tiền nhưng bị phát hiện thật khó để làm như vậy.
->>>>>>> feature/phat
               </p>
             </div>
             <img src="https://s3n.cashify.in/imageLibrary/IDEA_1e5022c3d5d8.png"></img>
           </div>
-<<<<<<< HEAD
-          <div className="flex flex-row w-3/4 m-6">
-            <img src="https://s3n.cashify.in/imageLibrary/2nd_Big_Image__42bac24a39f8.png"></img>
-            <div className="w-3/5 flex flex-col justify-center items-center m-6">
-              <h3 className="text-teal-400">
-                That's when the concept of CASHIFY was formed
-              </h3>
-              <p>
-                To create a PLATFORM that would let you Sell, Repair, Recycle &
-                Manage your phone in a SIMPLE MANNER for the Best PRICE.
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-row w-3/4 m-6">
-            <div className="w-3/5 flex flex-col justify-center items-center">
-              <h3 className="text-teal-400">
-                From 1 Room to 15 Office From 3 people to 800 People
-              </h3>
-              <p>
-                We are now a team of 800 PASSIONATE folks who are DEDICATED to
-                help you keep your smartphone (the most important thing in your
-                life) forever healthy & UP-TO-DATE.
-=======
           <div className="flex flex-row flex-wrap w-3/4 m-6">
             <img src="https://s3n.cashify.in/imageLibrary/2nd_Big_Image__42bac24a39f8.png"></img>
             <div className="w-3/5 flex flex-col justify-center items-center m-6">
@@ -197,22 +113,11 @@ const AboutPage = () => {
                 Chúng tôi hiện là một nhóm gồm 800 người ĐAM MÊ, những người
                 DÀNH RIÊNG cho giúp bạn giữ điện thoại thông minh của mình (thứ
                 quan trọng nhất trong life) mãi khỏe mạnh & CẬP NHẬT.
->>>>>>> feature/phat
               </p>
             </div>
             <img src="https://s3n.cashify.in/imageLibrary/3rd_Image_dc4813722c53.png"></img>
           </div>
-<<<<<<< HEAD
-          <div className="flex flex-row w-3/4 m-6">
-            <img src="https://s3n.cashify.in/imageLibrary/4th_Image__d7c26150f695.png"></img>
-            <div className="w-3/5 flex flex-col justify-center items-center">
-              <h3 className="text-teal-400">Today</h3>
-              <p>
-                Cashify not just ENABLES you to SELL but also, BUY, RECYCLE,
-                ACCESSORIZE & REPAIR your smartphone, so that every time you
-                visit us, you get all that you need
-=======
-          <div className="flex flex-wrap flex-row w-3/4 m-6">
+          <div className="flex flex-row flex-wrap w-3/4 m-6">
             <img src="https://s3n.cashify.in/imageLibrary/4th_Image__d7c26150f695.png"></img>
             <div className="w-3/5 flex flex-col justify-center items-center">
               <h3 className="text-violet-700">Hiện tại</h3>
@@ -220,7 +125,6 @@ const AboutPage = () => {
                 Cashify không chỉ CHO PHÉP bạn BÁN mà còn MUA, TÁI CHẾ, TRUY CẬP
                 & SỬA CHỮA điện thoại thông minh của bạn, để mỗi khi bạn hãy đến
                 với chúng tôi, bạn sẽ có được tất cả những gì bạn cần
->>>>>>> feature/phat
               </p>
             </div>
           </div>
@@ -228,25 +132,16 @@ const AboutPage = () => {
         <div className="staff-member">
           <h1 className="text-center">Team</h1>
           <h3 className="text-center">
-<<<<<<< HEAD
-            We’re here to unleash the world’s creative energy by designing a
-            more enlightened way of working.
-=======
             Chúng tôi ở đây để giải phóng năng lượng sáng tạo của thế giới bằng
             cách thiết kế một cách làm việc sáng suốt hơn.
->>>>>>> feature/phat
           </h3>
           <ul className="flex flex-row flex-wrap items-center justify-center">
             <li>
               <div className="flex justify-center items-center">
                 <section>
-<<<<<<< HEAD
-                  <p className="text-teal-400 font-semibold">MANDEEP MANOCHA</p>
-=======
                   <p className="text-violet-700 font-semibold">
                     MANDEEP MANOCHA
                   </p>
->>>>>>> feature/phat
                   <p>Co-Founder & CEO</p>
                 </section>
                 <img src="https://s3n.cashify.in/imageLibrary/Mandeep_a344d2b850de.png"></img>
@@ -255,11 +150,7 @@ const AboutPage = () => {
             <li>
               <div className="flex justify-center items-center">
                 <section>
-<<<<<<< HEAD
-                  <p className="text-teal-400 font-semibold">NAKUL KUMAR</p>
-=======
                   <p className="text-violet-700 font-semibold">NAKUL KUMAR</p>
->>>>>>> feature/phat
                   <p>Co-founder & CMO</p>
                 </section>
                 <img src="https://s3n.cashify.in/imageLibrary/Mask_Group_43_059081208fae.png"></img>
@@ -268,11 +159,7 @@ const AboutPage = () => {
             <li>
               <div className="flex justify-center items-center">
                 <section>
-<<<<<<< HEAD
-                  <p className="text-teal-400 font-semibold">AMIT SETHI</p>
-=======
                   <p className="text-violet-700 font-semibold">AMIT SETHI</p>
->>>>>>> feature/phat
                   <p>Co-Founder & CTO</p>
                 </section>
                 <img src="https://s3n.cashify.in/imageLibrary/Mask_Group_432x_e6bd21f54a9a.png"></img>
@@ -281,11 +168,7 @@ const AboutPage = () => {
             <li>
               <div className="flex justify-center items-center">
                 <section>
-<<<<<<< HEAD
-                  <p className="text-teal-400 font-semibold">
-=======
                   <p className="text-violet-700 font-semibold">
->>>>>>> feature/phat
                     SIDDHANT DHINGRA
                   </p>
                   <p>Co-founder & Chief Business </p>
@@ -297,11 +180,7 @@ const AboutPage = () => {
             <li>
               <div className="flex justify-center items-center">
                 <section>
-<<<<<<< HEAD
-                  <p className="text-teal-400 font-semibold">AKSH CHAUHAN</p>
-=======
                   <p className="text-violet-700 font-semibold">AKSH CHAUHAN</p>
->>>>>>> feature/phat
                   <p>COO</p>
                 </section>
                 <img src="https://s3n.cashify.in/imageLibrary/Image_1702x_15fc05ca2e65.png"></img>
@@ -310,11 +189,7 @@ const AboutPage = () => {
             <li>
               <div className="flex justify-center items-center">
                 <section>
-<<<<<<< HEAD
-                  <p className="text-teal-400 font-semibold">SHUBH DARPAN</p>
-=======
                   <p className="text-violet-700 font-semibold">SHUBH DARPAN</p>
->>>>>>> feature/phat
                   <p>Chief Revenue Officer</p>
                 </section>
                 <img src="https://s3n.cashify.in/imageLibrary/Image_1762x_6c8a508082cf.png"></img>
@@ -323,11 +198,7 @@ const AboutPage = () => {
             <li>
               <div className="flex justify-center items-center">
                 <section>
-<<<<<<< HEAD
-                  <p className="text-teal-400 font-semibold">
-=======
                   <p className="text-violet-700 font-semibold">
->>>>>>> feature/phat
                     ARUN PRATAP SINGH
                   </p>
                   <p>VP - Finance & Accounts</p>
@@ -340,11 +211,7 @@ const AboutPage = () => {
             <li>
               <div className="flex justify-center items-center">
                 <section>
-<<<<<<< HEAD
-                  <p className="text-teal-400 font-semibold">
-=======
                   <p className="text-violet-700 font-semibold">
->>>>>>> feature/phat
                     ALOK KUMAR SHUKLA
                   </p>
                   <p>Senior VP - HR & Retail</p>
@@ -355,11 +222,7 @@ const AboutPage = () => {
             <li>
               <div className="flex justify-center items-center">
                 <section>
-<<<<<<< HEAD
-                  <p className="text-teal-400 font-semibold">
-=======
                   <p className="text-violet-700 font-semibold">
->>>>>>> feature/phat
                     PANKAJ KUMAR AGRAWAL
                   </p>
                   <p>Senior VP - Engineering</p>
@@ -369,11 +232,7 @@ const AboutPage = () => {
             </li>
           </ul>
         </div>
-<<<<<<< HEAD
-        <div className="bg-teal-500 w-3/4 p-8 rounded-xl h-8">
-=======
         <div className="bg-violet-600 w-3/4  rounded-xl h-8 m-4">
->>>>>>> feature/phat
           <h1 className="text-center">COME JOIN US !</h1>
           <span>
             <i className="fa-sharp fa-solid fa-arrow-up-right-from-square"></i>
